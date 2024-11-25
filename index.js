6a0b34a0156aabb0baf00a4b3198009d3cf7a079
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-11-25 10:37:50 -0330

    Login with email (error not functional)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const app = express();
 const PORT = 3000;
@@ -10,69 +10,83 @@ const SALT_ROUNDS = 10;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-    session({
-        secret: "replace_this_with_a_secure_key",
-        resave: false,
-        saveUninitialized: true,
-    })
+  session({
+    secret: "my_secure_key",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 const USERS = [
-    {
-        id: 1,
-        username: "AdminUser",
-        email: "admin@example.com",
-        password: bcrypt.hashSync("admin123", SALT_ROUNDS), //In a database, you'd just store the hashes, but for 
-                                                            // our purposes we'll hash these existing users when the 
-                                                            // app loads
-        role: "admin",
-    },
-    {
-        id: 2,
-        username: "RegularUser",
-        email: "user@example.com",
-        password: bcrypt.hashSync("user123", SALT_ROUNDS),
-        role: "user", // Regular user
-    },
+  {
+    id: 1,
+    username: "AdminUser",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("admin123", SALT_ROUNDS), //In a database, you'd just store the hashes, but for
+    // our purposes we'll hash these existing users when the
+    // app loads
+    role: "admin",
+  },
+  {
+    id: 2,
+    username: "RegularUser",
+    email: "user@example.com",
+    password: bcrypt.hashSync("user123", SALT_ROUNDS),
+    role: "user", // Regular user
+  },
 ];
 
 // GET /login - Render login form
 app.get("/login", (request, response) => {
-    response.render("login");
+  const errorMessage = request.query.error || null;
+  response.render("login", { errorMessage });
 });
 
 // POST /login - Allows a user to login
 app.post("/login", (request, response) => {
+  const { email, password } = request.body;
+  const user = USERS.find((user) => user.email === email);
 
+  if (!!user && bcrypt.compareSync(password, user.password)) {
+    request.session.email = email;
+    return response.redirect("/");
+  }
+
+  //Error logging in, redirect back to login with error message
+  return response
+    .status(400)
+    .redirect("/login?error=Invalid username or password.");
 });
 
 // GET /signup - Render signup form
 app.get("/signup", (request, response) => {
-    response.render("signup");
+  response.render("signup");
 });
 
 // POST /signup - Allows a user to signup
 app.post("/signup", (request, response) => {
-    
+  const { username, email, password } = request.body;
 });
 
 // GET / - Render index page or redirect to landing if logged in
 app.get("/", (request, response) => {
-    if (request.session.user) {
-        return response.redirect("/landing");
-    }
-    response.render("index");
+  const userEmail = request.session.email;
+  const errorMessage = request.query.error || null;
+
+  if (request.session.user) {
+    return response.redirect("/landing");
+  }
+
+  response.render("index", { userEmail, errorMessage });
 });
 
 // GET /landing - Shows a welcome page for users, shows the names of all users if an admin
-app.get("/landing", (request, response) => {
-    
-});
+app.get("/landing", (request, response) => {});
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
